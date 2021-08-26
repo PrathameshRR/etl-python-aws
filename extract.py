51b065068d83a251d371bfc15b4317667f120e11
Prathamesh Patel <prathamesh.patel15@gmail.com>
2021-08-27 01:56:26 +0530

    raw csv download and print using python

--- extract.py
+++ extract.py
@@ -4,15 +4,22 @@ import pandas as pd
 import requests
 import io
 
-# Downloading the csv file from your GitHub account
+# Downloading the NYT csv file from GitHub
 
-url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv"  # Make sure the url is the raw version of the file on GitHub
-download = requests.get(url).content
+urlNYT = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv"  # Make sure the url is the raw version of the file on GitHub
+downloadNYT = requests.get(urlNYT).content
+
+# Downloading the John Hopkins csv file from GitHub
+
+urlJH = "https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv"  # Make sure the url is the raw version of the file on GitHub
+downloadJH = requests.get(urlJH).content
 
 # Reading the downloaded content and turning it into a pandas dataframe
+dfNYT = pd.read_csv(io.StringIO(downloadNYT.decode('utf-8')),index_col=0, parse_dates=True)
 
-df = pd.read_csv(io.StringIO(download.decode('utf-8')))
-
+dfJH = pd.read_csv(io.StringIO(downloadJH.decode('utf-8')),index_col=0, parse_dates=True)
 # Printing out the first 5 rows of the dataframe
+# print(df)
 
-print(df)
+dfNYT.info()
+dfJH.info()
