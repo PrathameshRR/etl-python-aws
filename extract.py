152862e3b80d492396f4c6d007b137b18e2f3792
Prathamesh Patel <prathamesh.patel15@gmail.com>
2021-09-08 15:41:41 +0530

    Filtering and merging complete

--- extract.py
+++ extract.py
@@ -15,11 +15,30 @@ jh_csv_url = "https://raw.githubusercontent.com/datasets/covid-19/master/data/ti
 download_jh_data = requests.get(jh_csv_url).content
 
 # Reading the downloaded content and turning it into a pandas dataframe
-data_frame_nyt = pd.read_csv(io.StringIO(download_nyt_data.decode('utf-8')),index_col=0, parse_dates=True)
+data_frame_nyt = pd.read_csv(io.StringIO(download_nyt_data.decode('utf-8')))
 
-data_frame_jh = pd.read_csv(io.StringIO(download_jh_data.decode('utf-8')),index_col=0, parse_dates=True)
+data_frame_jh = pd.read_csv(io.StringIO(download_jh_data.decode('utf-8')))
 # Printing out the first 5 rows of the dataframe
-# print(df)
+# print(df)`
 
-data_frame_nyt.info()
-data_frame_jh.info()
+# print(data_frame_nyt.head())
+# print(data_frame_jh.head())
+
+##print(data_frame_jh["Country/Region" == "US"])
+
+#us_data_jh_almost_final = data_frame_jh[data_frame_jh["Country/Region"] == "US"]
+
+#us_data_jh_final = us_data_jh_almost_final.loc['Date', 'Recovered']
+# us_data_jh_final = us_data_jh_almost_final.drop(['Confirmed', 'Deaths', 'Province/State','Country/Region'], inplace=True, axis=1)
+data_frame_jh.columns = ['Date','Country', 'State', 'Confirmed', 'Recovered', 'Deaths']
+data_frame_nyt.columns = ['Date','Cases','Deaths']
+
+df = data_frame_jh[data_frame_jh.Country == "US"]
+
+dff = df.loc[:,['Date','Recovered']]
+
+Inner_Join = pd.merge(dff,data_frame_nyt, how='inner', on=['Date', 'Date'])
+print(Inner_Join)
+
+
+#print(dff)
