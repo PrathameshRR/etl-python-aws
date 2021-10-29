5dfd8c6109052de0ca5f8eb35ccbee22b1c48682
Prathamesh Patel <prathamesh.patel15@gmail.com>
2021-10-29 10:51:37 +0530

    python practise

---
+++ basicmath.py
@@ -0,0 +1,34 @@
+a = int(input("Enter a number: "))
+
+b = int(input("Enter another number: "))
+
+if __name__ == '__main__':
+
+    def add_num(a,b):
+        return a + b
+print(add_num(a,b))
+
+def mul_num(a,b):
+    return a * b
+print(mul_num(a,b))
+
+def sub_num(a,b):
+    return a - b
+print(sub_num(a,b))
+
+def div_num(a,b):
+    return a / b
+print(div_num(a,b))
+
+def mod_num(a,b):
+    return a % b
+print(mod_num(a,b))
+
+def pow_num(a,b):
+    return a ** b
+print(pow_num(a,b))
+
+def floordiv_num(a,b):
+    return a // b
+print(floordiv_num(a,b))
+
